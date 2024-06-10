42407d72e9be81eec625ff20434eb2ff404e402e
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-06-10 17:00:54 +0200

    index8

--- js/film.js
+++ js/film.js
@@ -4,8 +4,16 @@ const id = paremeters.get("id");
 if(id == null || id.trim() === "")window.location = "index.html";
 const API_KEY = "b17f4ca5";
 let movie;
-getMovie(id).then(data=>console.log(data));
-console.log(movie);
+getMovie(id).then(data=>{
+    movie = data;
+    console.table(movie);
+    
+
+    const movieDiv = document.querySelector("article.main");
+    movieDiv.querySelector('img').src = movie.Poster;
+    movieDiv.querySelector('.title').innerHTML = movie.Title;
+
+});
 
 /**
  * 
@@ -19,7 +27,7 @@ async function getMovie(imdbID){
         const json = response.json();
         let results;
         await json.then(data => results = data);
-        movie = results;
+        return results;
     }catch(error){
         console.error("error",error);
         return;
