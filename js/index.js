b187bbe09e29dfc61a200b39a2beec5a5d710788
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-06-10 15:31:57 +0200

    index4

--- js/index.js
+++ js/index.js
@@ -4,4 +4,15 @@ searchBtn.addEventListener("click",()=>{
     let value = searchInput.value;
     if(value.trim() === "")return;
     else if(isSearching)return;
+    const API_KEY = "b17f4ca5";
+    const url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${value}`;
+    console.log(url);
 })
+
+/**
+ * 
+ * @param {string} link 
+ */
+async function generateMovies(link){
+
+}
